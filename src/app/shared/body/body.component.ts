90ac417c322f736092d27f53de78d05ca3db0a81
frodarte <Gerardo.Bucio@gmail.com>
2019-03-05 16:46:35 -0600

    Hemos creado nuestros primeros componentes

---
+++ src/app/shared/body/body.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent implements OnInit {
+
+  public description: string ="Esta es la descripciòn";
+  public price: number= 12.223;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
